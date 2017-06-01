fff089fe0a99e393977acf55fe168cc3ccf3795e
Carlosl93 <Carlosl93>
2017-06-01 09:02:10 -0430

    Deployment

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/nodekb',
+  //database: 'mongodb://localhost:27017/nodekb',
+  database: 'mongodb://<dbuser>:<dbpassword>@ds161001.mlab.com:61001/nodekb',
   secret: 'nosecret'
 }
